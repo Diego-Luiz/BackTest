17b143069d703797ecff5f3c973024d7486f2cbf
Diego Luiz <51066402+Diego-Luiz@users.noreply.github.com>
2020-04-06 15:26:44 -0400

    Erro arrumado
    Erro estava na atribuição de dados, no metodo __init__ da classe da estratégia

--- back.py
+++ back.py
@@ -7,109 +7,110 @@ import sys
 
 import backtrader as bt
 
-class Test(bt.Strategy):
+class TestStrategy(bt.Strategy):
     params = (
-        ('period', 5),
+        ('maperiod', 5),
     )
 
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+
     def __init__(self):
-        self.data = self.datas[0]
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+
+        # To keep track of pending orders and buy price/commission
         self.order = None
         self.buyprice = None
         self.buycomm = None
-        # Add SimpleMovingAverage indicator for use in the trading strategy
-        self.sma = bt.indicators.SMA(self.data[0],period=self.p.period)
-        
-    def log(self,txt,dt=None):
-        dt= dt or self.data.datetime.date(0)
-        print('%s %s' % (dt.isoformat(), txt))
-    
+
+        # Add a MovingAverageSimple indicator
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], period=self.params.maperiod)
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+        # Check if an order has been completed
+        # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:  # Sell
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
     def next(self):
-        print('no next')
-        self.log('Close, %.2f' % self.data.close[0])
+        # Simply log the closing price of the series from the reference
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
         if self.order:
-            return 
-    
+            return
+
+        # Check if we are in the market
         if not self.position:
-            print('not self.position')
+
             # Not yet ... we MIGHT BUY if ...
-            if self.data.close[0] > self.sma[0]:
-                print('here')
+            if self.dataclose[0] > self.sma[0]:
+
                 # BUY, BUY, BUY!!! (with all possible default parameters)
-                self.log('BUY CREATE, %.2f' % self.data.close[0])
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
 
                 # Keep track of the created order to avoid a 2nd order
-                self.order = self.buy()       
+                self.order = self.buy()
 
         else:
-            print('else')
-            if self.data.close[0] < self.sma[0]:
+
+            if self.dataclose[0] < self.sma[0]:
                 # SELL, SELL, SELL!!! (with all possible default parameters)
                 self.log('SELL CREATE, %.2f' % self.dataclose[0])
 
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.sell()
 
-    def notify_order(self,order):           
-        if order.status in [order.Submitted, order.Accepted]: #se a ordem foi submetida para o broker ou foi aceita pelo broker
-            return
-        
-        if order.status in [order.Completed]: #se a ordem foi completamente executada pelo broker
-            if order.isbuy():
-                self.log('Buy executed, Price: %.2f, Cost: %.2f, Comm: %.2f' % (order.executed.price, order.executed.value, order.executed.comm) )
-                self.buyprice = order.executed.price
-                self.comm = order.executed.comm
-
-            else:
-                self.log('Sell executed, Price: %.2f, Cost: %.2f, Comm: %.2f' % (order.executed.price, order.executed.value, order.executed.comm) )
-
-            self.bar_executed = len(self)
-        
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            self.log('Order Canceled/Margin/Rejected')
-
-        self.order = None
-    
-    def notify_trade(self, trade):
-        if not trade.isclosed:
-            return
-        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' % (trade.pnl, trade.pnlcomm))
 
 if __name__ == "__main__":
     cerebro = bt.Cerebro()
 
-    datapath = 'MSFT.csv'
-    # data = bt.feeds.GenericCSVData(
-    #     dataname = datapath,
-    #     fromdate = datetime.datetime(2019,10,21),
-    #     todate = datetime.datetime(2020,3,23),
-        
-    #     dtformat = ('%Y-%m-%d'),
-    #     nullvalue = 0.0,
-    #     datetime = 0,
-    #     open = 1,
-    #     high = 2,
-    #     low = 3,
-    #     close = 4,
-    #     volume = 6,
-    #     openinterest = -1
-    # )
-
     data = bt.feeds.YahooFinanceCSVData(
-        dataname= 'MSFT.csv',
-        fromdate=datetime.datetime(2019, 10, 21)
+        dataname= 'MSFT.csv'
     )
     
     cerebro.adddata(data)
-    cerebro.addstrategy(Test)
-    cerebro.broker.set_cash(100000.0)
+    cerebro.addstrategy(TestStrategy)
+    cerebro.broker.set_cash(1000.0)
 
-    # Add a FixedSize sizer according to the stake
-    # cerebro.addsizer(bt.sizers.FixedSize, stake=10)
-    # Set the commission - 0.1% ... divide by 100 to remove the %
     cerebro.broker.setcommission(commission=0.0)
     
     print('Initial value: %.2f' % cerebro.broker.get_value())
     cerebro.run()
     print('Final value: %.2f' % cerebro.broker.get_value())
-    # cerebro.plot()
+    cerebro.plot()
